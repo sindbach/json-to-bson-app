d8dc37fc2017a631d2e063ecdc8edaaac184d5e1
Wan Bachtiar <sindbach@gmail.com>
2021-02-24 17:18:47 +1100

    Added Form Controls

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -3,18 +3,29 @@ import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import PropTypes from "prop-types";
 import CssBaseline from '@material-ui/core/CssBaseline';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import FormContainer from "./FormContainer";
+import TransformIcon from '@material-ui/icons/Transform';
 
 const styles = theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  appbar: {
+    backgroundColor: theme.palette.info.dark 
+  },
   content: {
     padding: theme.spacing(1),
   },
-  toolbar: theme.mixins.toolbar,
   typography: {
     fontSize:18,
-  }
+  },
+  title: {
+    flexGrow: 1,
+  },
 });
+
 
 class MainContainer extends Component {
   constructor(props) {
@@ -24,10 +35,18 @@ class MainContainer extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
+      <div>
         <CssBaseline />
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
+            <AppBar position="static" className={classes.appbar}>
+              <Toolbar>
+                <TransformIcon className={classes.icon} />
+                <Typography variant="h6" className={classes.title}>
+                  JSON To BSON 
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          <br/><br/>
+          <main className={classes.content}>
           <Typography className={classes.typography} paragraph>
             Convert JSON to Go BSON Class Maps.
           </Typography>
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainContainer from "./components/MainContainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
